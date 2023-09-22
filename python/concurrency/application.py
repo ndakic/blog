fe9470a38d83cf75cd23d380902e131297e957ec
ndakic <ndakic94@gmail.com>
2023-09-22 18:52:47 +0200

    examples update

--- python/concurrency/application.py
+++ python/concurrency/application.py
@@ -1,85 +1,116 @@
-from util import sum_square, download_image, timing, run_computing_function, run_io_function
+from util import sum_square, download_image, timing, load_image_urls
 from util import IMAGE_FOLDER
-from concurrent.futures import ThreadPoolExecutor
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+
+IMAGE_URLS = load_image_urls()
 
 
 @timing
-def use_multiprocessing_on_computing_function():
+def sum_square_serial_run():
+    result = sum_square((0, 400_000_000))
+    return result
+
+
+@timing
+def sum_square_multiprocessing_run():
+    with ProcessPoolExecutor() as pp_executor:
+        mp_results = pp_executor.map(sum_square,
+                                     [(0, 10000000),
+                                      (10000000, 20000000),
+                                      (20000000, 30000000),
+                                      (30000000, 40000000)])
+        mp_final_result = sum(mp_results)
+        return mp_final_result
+
+
+@timing
+def sum_square_multithreading_run():
+    with ThreadPoolExecutor() as tp_executor:
+        mt_results = tp_executor.map(sum_square,
+                                     [(0, 10000000),
+                                      (10000000, 20000000),
+                                      (20000000, 30000000),
+                                      (30000000, 40000000)])
+        mt_final_result = sum(mt_results)
+        return mt_final_result
+
+
+@timing
+def download_images_serial_run():
+    for i, img_url in enumerate(IMAGE_URLS):
+        download_image(img_url, IMAGE_FOLDER / "serial", f"image_{i}.jpg")
+
+
+@timing
+def download_images_multiprocessing_run():
     with ProcessPoolExecutor() as executor:
-        results = executor.map(sum_square, [100_000_000, 100_000_000, 100_000_000, 100_000_000])
+        for i, url in enumerate(IMAGE_URLS):
+            executor.submit(download_image, url, IMAGE_FOLDER / "mp", f"image_{i}.jpg")
 
 
 @timing
-def use_multiprocessing_on_io_function():
-    with open(IMAGE_FOLDER / "images.txt", "r") as f:
-        img_urls = f.readlines()
-    with ProcessPoolExecutor() as executor:
-        for url in img_urls:
-            executor.submit(download_image, url, IMAGE_FOLDER)
-
-
-@timing
-def use_multithreading_on_computing_function():
+def download_images_multithreading_run():
     with ThreadPoolExecutor() as executor:
-        results = executor.map(sum_square, [100_000_000, 100_000_000, 100_000_000, 100_000_000])
-
-
-@timing
-def use_multithreading_on_io_function():
-    with open(IMAGE_FOLDER / "images.txt", "r") as f:
-        img_urls = f.readlines()
-
-    with ThreadPoolExecutor() as executor:
-        for url in img_urls:
-            executor.submit(download_image, url, IMAGE_FOLDER)
+        for i, url in enumerate(IMAGE_URLS):
+            executor.submit(download_image, url, IMAGE_FOLDER / "mt", f"image_{i}.jpg")
 
 
 if __name__ == "__main__":
     print("========================================================")
-    print("\t\tComputing function started..")
-    run_computing_function()
-    print("Use multiprocessing on computing function started..")
-    use_multiprocessing_on_computing_function()
-    print("Use multithreading on computing function started..")
-    use_multithreading_on_computing_function()
-    print("\t\tTesting io function started")
-    run_io_function()
-    print("Testing multiprocessing on io function started")
-    use_multiprocessing_on_io_function()
-    print("Testing multithreading on io function started")
-    use_multithreading_on_io_function()
+    print("\tsum_square (CPU-bound function) function started..")
+    print("========================================================")
+    result_serial = sum_square_serial_run()
+    print("\tsum_square function with multiprocessing started..")
+    print("========================================================")
+    result_multiprocessing = sum_square_multiprocessing_run()
+    print(" sum square function with multithreading started..")
+    print("========================================================")
+    result_multithreading = sum_square_multithreading_run()
+    assert result_serial == result_multiprocessing == result_multithreading
+    print("\t\tResult of all functions are equal!")
+    print("==========================================================")
+    print("\t download_images (I/O function) function started..")
+    download_images_serial_run()
+    print("\t download_images function with multiprocessing started..")
+    download_images_multiprocessing_run()
+    print("\t download_images function with multithreading started..")
+    download_images_multithreading_run()
 
     """
         Results on my machine:
-        ========================================================
-                Computing function started..
-        =====================================================
-        Finished in 15.79 seconds
-        =====================================================
-        Use multiprocessing on computing function started..
-        =====================================================
-        Finished in 4.42 seconds
-        =====================================================
-        Use multithreading on computing function started..
-        =====================================================
-        Finished in 15.47 seconds
-        =====================================================
-                Testing io function started
-        =====================================================
-        Finished in 19.07 seconds
-        =====================================================
-        Testing multiprocessing on io function started
-        =====================================================
-        Finished in 2.78 seconds
-        =====================================================
-        Testing multithreading on io function started
-        =====================================================
-        Finished in 1.47 seconds
-        =====================================================
+    ========================================================
+         sum_square (CPU-bound function) function started..
+    ========================================================
+    ==========================================================
+                Finished in 20.89 seconds
+    ==========================================================
+        sum_square function with multiprocessing started..
+    ========================================================
+    ==========================================================
+                Finished in 0.61 seconds
+    ==========================================================
+       sum square function with multithreading started..
+    ========================================================
+    ==========================================================
+                Finished in 2.02 seconds
+    ==========================================================
+            Result of all functions are equal!
+    ==========================================================
+         download_images (I/O function) function started..
+    ==========================================================
+                Finished in 33.26 seconds
+    ==========================================================
+      download_images function with multiprocessing started..
+    ==========================================================
+                Finished in 5.54 seconds
+    ==========================================================
+      download_images function with multithreading started..
+    ==========================================================
+                Finished in 3.77 seconds
+    ==========================================================
         
         Conclusion: As we can see, multiprocessing is faster than multithreading in computing functions, 
                     but in IO functions, multithreading is faster than multiprocessing.
-                    Use multiprocessing when you have a lot of computing tasks, and use multithreading when you have a 
-                    lot of IO tasks.
+                    Use multiprocessing when you have a computing tasks, and use multithreading when you have a I/O
+                    tasks.
     """
--- python/concurrency/util.py
+++ python/concurrency/util.py
@@ -1,30 +1,26 @@
-import io
 import requests
 import time
-import numpy as np
 from functools import wraps
-from PIL import Image
 from pathlib import Path
 
 IMAGE_FOLDER = Path(".").absolute() / "files"
 
 
-# computing function
-def sum_square(x: int) -> int:
+# CPU-bound function
+def sum_square(custom_range: tuple) -> int:
     final = 0
-    for i in range(x):
+    for i in range(custom_range[0], custom_range[1]):
         final += i * i
     return final
 
 
 # I/O function
-def download_image(img_url: str, save_loc: Path) -> np.ndarray:
+def download_image(img_url: str, save_loc: Path, image_name):
     img_url = img_url.replace("\n", "")
     img_bytes = requests.get(img_url).content
     save_loc.mkdir(parents=True, exist_ok=True)
-    with open(save_loc / "image", "wb") as img_file:
+    with open(save_loc / image_name, "wb") as img_file:
         img_file.write(img_bytes)
-    return np.array(Image.open(io.BytesIO(img_bytes)))
 
 
 def timing(func):
@@ -33,24 +29,14 @@ def timing(func):
         start = time.perf_counter()
         func(*args, **kwargs)
         finish = time.perf_counter()
-        print("=====================================================")
-        print(f"Finished in {round(finish - start, 2)} seconds")
-        print("=====================================================")
+        print("==========================================================")
+        print(f"\t\t\tFinished in {round(finish - start, 2)} seconds")
+        print("==========================================================")
 
     return wrapper
 
 
-@timing
-def run_computing_function():
-    sum_square(100_000_000)
-    sum_square(100_000_000)
-    sum_square(100_000_000)
-    sum_square(100_000_000)
-
-
-@timing
-def run_io_function():
-    with open(IMAGE_FOLDER / "images.txt", "r") as urls_file:
-        urls = urls_file.readlines()
-    for url in urls:
-        download_image(url, IMAGE_FOLDER)
+def load_image_urls():
+    with open(IMAGE_FOLDER / "images.txt", "r") as f:
+        img_urls = f.readlines()
+    return img_urls
